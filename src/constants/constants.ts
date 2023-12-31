66296694d758a6b870fffb7ce4345988d65823e0
arstro <lil_ham0n@hotmail.com>
2023-12-30 22:32:23 -0600

    first commit

---
+++ src/constants/constants.ts
@@ -0,0 +1,43 @@
+export const navbarlinks = [
+    {
+        title: "Home", 
+        icon:"../../public/star-fall-2-svgrepo-con.svg",
+        link: "/"
+    },
+    {
+        title: "Portfolio", 
+        link: "/Portfolio"
+    },
+    {
+        title: "Listen", 
+        link: "/Listen"
+    },
+    {
+        title: "Watch", 
+        link: "/Watch"
+    },
+    {
+        title: "About", 
+        link: "/About"
+    },
+    {
+        title: "Shop", 
+        link: "/Shop"
+    },
+
+];
+
+export const footerLinks = [
+    {
+        title: "Privacy Policy", 
+        link: "/privacy"
+    },
+    {
+        title: "Terms and Conditions",  
+        link: "/terms"
+    },
+    {
+        title: "Use My Data",  
+        link: "/data"
+    }
+];
